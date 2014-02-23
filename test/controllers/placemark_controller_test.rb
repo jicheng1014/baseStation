1f2dd7478b0cb23866a41eeee8404c0e641a72e6
jicheng1014 <jicheng1014@gmail.com>
2014-02-23 10:21:36 +0800

    增加了测试的跳转核对

--- test/controllers/placemark_controller_test.rb
+++ test/controllers/placemark_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class PlacemarkControllerTest < ActionController::TestCase
   test "should get import" do
     get :import
-    assert_response :success
+    assert_redirected_to  stations_path
   end
 
 end
